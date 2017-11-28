33ecb511523d2c99abbf999c2db10758e3aaeb80
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2017-11-27 18:32:12 -0600

    Create frames table.

--- assignment3.cpp
+++ assignment3.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <iomanip>
 #include <regex>
 #include <string>
 #include <fstream>
+#include <list>
+#include <cmath>
 
 using namespace std;
 
+/*
+ * TO-DO!
+ * Create Semaphore
+ * Implement Algorithms
+ * Create Disk Scheduler
+ */
+
 struct processObject {
 	int pid = 0;
-	char temp[2];
+	int size = 0;
+};
+
+struct memoryRequest {
+	int segment = 0; //Segment #. In this case PID is the segment #.
+	int page = 0; //Page #
+	int offset = 0; //Offset
+};
+
+struct frame {
+	int a = 0; //Address Space Number/PID
+	int p = 0; //Page Number
 };
 
 int main(int argc, char **argv) {
@@ -19,13 +40,14 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 	
-	int totalPages = 0; //Total amount of pages
+	int totalPages = 0; //Total amount of page frames
 	int segmentLength = 0; //Segment length
 	int pageSize = 0; //Size of a page
 	int framesPerProcess = 0; //Frames per process
 	int x = 0; //Used for LRU-X and OPT-X
 	int min = 0, max = 0;
 	int processCount = 0;
+	
 	
 	string bufferString = ""; //Used for parsing
 	regex pidSize("(\\d+)\\s+(\\d+)", regex::ECMAScript);
@@ -35,54 +57,104 @@ int main(int argc, char **argv) {
 	
 	getline(commandFile, bufferString);
 	totalPages = stoi(bufferString);
-	cout << totalPages << endl;
+
 	getline(commandFile, bufferString);
 	segmentLength = stoi(bufferString);
-	cout << segmentLength << endl;
+
 	getline(commandFile, bufferString);
 	pageSize = stoi(bufferString);
-	cout << pageSize << endl;
+
 	getline(commandFile, bufferString);
 	framesPerProcess = stoi(bufferString);
-	cout << framesPerProcess << endl;
+
 	getline(commandFile, bufferString);
 	x = stoi(bufferString);
-	cout << x << endl;
+
 	getline(commandFile, bufferString);
 	min = stoi(bufferString);
-	cout << min << endl;
+
 	getline(commandFile, bufferString);
 	max = stoi(bufferString);
-	cout << max << endl;
+
 	getline(commandFile, bufferString);
 	processCount = stoi(bufferString);
-	cout << processCount << endl;
-	
+
+	//Data Structures Needed
 	processObject processList[processCount];
+	list<memoryRequest> pageRequests;
+	list<frame> framesTable;
+	
+	frame emptyFrame;
+	framesTable.assign(totalPages, emptyFrame);
 	
 	for (int i = 0; i < processCount; i++) {
-		//getline(commandFile, bufferString);
-		//cout << bufferString << endl;
-		getline(commandFile, bufferString);
-		regex_search(bufferString, matchResults, pidSize);
-		cout << "PID: " << matchResults[1].str() << " Size: "
-		<< matchResults[2].str() << endl;
+		getline(commandFile, bufferString); //Grab the line from the file
+		regex_search(bufferString, matchResults, pidSize); //Grab PID and Size
+		
+		int tempPID = stoi(matchResults[1].str());
+		int tempSize = stoi(matchResults[2].str());
+		
+		/*cout << "PID: " << matchResults[1].str() << " Size: "
+		<< matchResults[2].str() << endl;*/
+		
+		processList[tempPID - 100].pid = tempPID;
+		processList[tempPID - 100].size = tempSize;
 	}
 	
+	cout << "Total Number of Pages: " << totalPages << " "
+		 << "Segment Length: " << segmentLength << " "
+		 << "Page Size: " << pageSize << " "
+		 << "Frames Per Process: " << framesPerProcess << " "
+		 << "X: " << x << " "
+		 << "Min: " << min << " " << "Max: " << max << " "
+		 << "Process Count: " << processCount << endl << endl;
+	
+	/*for (int i = 0; i < processCount; i++) {
+		cout << "PID: " << processList[i].pid << " SIZE: " << processList[i].size << endl << endl;
+	}*/
+	
 	while(getline(commandFile, bufferString)) {
-		//cout << bufferString << endl;
+
 		if (regex_search(bufferString, matchResults, end)) {
-			cout << "PID: " << matchResults[1].str() <<
-			" Result: " << matchResults[2].str() << endl;			
+			memoryRequest temp;
+			temp.segment = stoi(matchResults[1].str());
+			temp.offset = -1;
+			
+			pageRequests.insert(pageRequests.end(), temp);
 		}
 		else {
 			regex_search(bufferString, matchResults, pidAddr);
-			for (int i = 1; i < matchResults.size(); i++) {
-				cout << matchResults[i].str() << " ";
-			}
-			cout << endl;
+			
+			int tempAddress = stoi(matchResults[2].str(), nullptr, 16);
+			cout << "HEX: 0x" << matchResults[2].str() << " DEC: " << tempAddress << endl;
+			memoryRequest temp;
+			temp.segment = stoi(matchResults[1].str());
+			temp.page = tempAddress/pageSize;
+			temp.offset = tempAddress - (temp.page * pageSize);
+			//temp.offset = (temp.page > 0 ? tempAddress : tempAddress%temp.page);
+			
+			pageRequests.insert(pageRequests.end(), temp);
 		}
+	}
+	cout << endl;
+	
+	cout << "Request Count: " << pageRequests.size() << endl;
+	
+	for (list<memoryRequest>::iterator itr = pageRequests.begin(); itr != pageRequests.end(); ++itr) {
+		cout << "Segment: " << itr->segment << " "
+			 << "Page: " << setw(2) << itr->page << " "
+			 << "Offset: " << setw(3) << itr->offset
+			 << endl;
+	}
+	cout << endl;
+	
+	cout << "Frames Table" << endl;
+	for (list<frame>::iterator itr = framesTable.begin(); itr != framesTable.end(); ++itr) {
+		cout << "Address Space: " << setw(4) << itr->a << " "
+			 << "Page #: " << setw(3) << itr->p
+			 << endl;
 	}
 	
 	return 0;
 }
+
