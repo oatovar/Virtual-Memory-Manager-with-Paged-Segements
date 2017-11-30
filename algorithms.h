1ffc6911960745d026ed0b29115ffb8255330006
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2017-11-30 15:28:49 -0600

    Update FIFO algorithm.

--- algorithms.h
+++ algorithms.h
@@ -11,29 +11,59 @@ void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, proces
 {
 	list<memoryRequest>::iterator requestItr = currentRequests.begin();
 	list<frame>::iterator frameItr = currentFrames.begin();
-	int addressSpace = requestItr->segment - 100;
+	int addressSpace = requestItr->addressSpace - 100;
 	while (requestItr != currentRequest.end()) {
-		if (requestItr->segment == frameItr->a) {
+        //If you run into the last request for the process just exit out
+        if ((requestItr->addressSpace == processNum) && (requestItr->offset == -1)) {
+            break;
+        }
+        //Check to see if the next request belongs to the process
+		else if ((requestItr->addressSpace == processNum) && (requestItr->offset != -1)) {
 			if (processProperties[addressSpace].currentFrameCount < framesPerProcess) {
 				while(frameItr != currentFrames.end()) {
-					if (frameItr->a == 0) {
-						frameItr->a = requestItr->segment
+					if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
+                        //Frame already contains page needed. No need to use a new frame buffer.
+                        break;
 					}
+                    else if (frameItr->addressSpace == 0) {
+                        //Copy page to the frame buffer
+                        cout << "Copying page into frame buffer." << endl;
+                        frameItr->addressSpace = requestItr->addressSpace;
+                        frameItr->segment = requestItr->segment;
+                        frameItr->page = requestItr->page;
+                        //Increase the count of frames that currently belong to the process
+                        processProperties[addressSpace].currentFrameCount++;
+                        //Break out of loop and move on to the next request
+                        break;
+                    }
 					else {
+                        //If frame occupied by another page either by the same process or another process
 						frameItr++; //Advance to next frame.
 					}
 				}
 			} else {
 				while(frameItr != currentFrames.end()) {
-					if (frameItr->a == requestItr->segment) {
-						//do replacement stuff
+					if (frameItr->addressSpace == requestItr->addressSpace) {
+                        cout << "Replacing frame buffer." << endl;
+						//Remove the first page that came in
+                        frameItr = currentFrames.erase(frameItr);
+                        //Update frames table to reflect the new order of pages in frame buffers
+                        frame newFrame;
+                        newFrame.addressSpace = requestItr->addressSpace;
+                        newFrame.segment = requestItr->segment;
+                        newFrame.page = requestItr->page;
+                        currentFrames.push_back(newFrame);
+                         //Break out of loop and move on to the next request
+                        break;
 					}
 					else {
 						frameItr++;
 					}
 				}
 			}
-		} else {
+		}
+        //If the request does not belong to the process move to the next process
+        else {
 			requestItr++;
 		}
 	}
