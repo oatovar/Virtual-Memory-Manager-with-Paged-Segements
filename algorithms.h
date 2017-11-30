c240cc0b7dd1cbb4bdff8dfa68425225538139d7
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2017-11-30 17:08:04 -0600

    Include LIFO algorithm.

--- algorithms.h
+++ algorithms.h
@@ -53,7 +53,7 @@ void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, proces
                         newFrame.segment = requestItr->segment;
                         newFrame.page = requestItr->page;
                         currentFrames.push_back(newFrame);
-                         //Break out of loop and move on to the next request
+                        //Break out of loop and move on to the next request
                         break;
 					}
 					else {
@@ -74,8 +74,66 @@ void lru()
 
 }
 
-void lifo()
+void lifo(list<frame> currentFrames, list<memoryRequest> currentRequests, processObject processProperties[], int framesPerProcess, int processNum)
 {
+    list<memoryRequest>::iterator requestItr = currentRequests.begin();
+    list<frame>::iterator frameItr = currentFrames.end();
+    int addressSpace = requestItr->addressSpace - 100;
+    while (requestItr != currentRequest.end()) {
+        //If you run into the last request for the process just exit out
+        if ((requestItr->addressSpace == processNum) && (requestItr->offset == -1)) {
+            break;
+        }
+        //Check to see if the next request belongs to the process
+        else if ((requestItr->addressSpace == processNum) && (requestItr->offset != -1)) {
+            if (processProperties[addressSpace].currentFrameCount < framesPerProcess) {
+                while(frameItr != currentFrames.begin()) {
+                    if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
+                        //Frame already contains page needed. No need to use a new frame buffer.
+                        break;
+                    }
+                    else if (frameItr->addressSpace == 0) {
+                        //Copy page to the frame buffer
+                        cout << "Copying page into frame buffer." << endl;
+                        frameItr->addressSpace = requestItr->addressSpace;
+                        frameItr->segment = requestItr->segment;
+                        frameItr->page = requestItr->page;
+                        //Increase the count of frames that currently belong to the process
+                        processProperties[addressSpace].currentFrameCount++;
+                        //Break out of loop and move on to the next request
+                        break;
+                    }
+                    else {
+                        //If frame occupied by another page either by the same process or another process
+                        frameItr--; //Advance to next frame. In this case moves on to an older frame.
+                    }
+                }
+            } else {
+                while(frameItr != currentFrames.begin()) {
+                    if (frameItr->addressSpace == requestItr->addressSpace) {
+                        cout << "Replacing frame buffer." << endl;
+                        //Remove the first page that came in
+                        frameItr = currentFrames.erase(frameItr);
+                        //Update frames table to reflect the new order of pages in frame buffers
+                        frame newFrame;
+                        newFrame.addressSpace = requestItr->addressSpace;
+                        newFrame.segment = requestItr->segment;
+                        newFrame.page = requestItr->page;
+                        currentFrames.push_front(newFrame);
+                        //Break out of loop and move on to the next request
+                        break;
+                    }
+                    else {
+                        frameItr--;//Go to the next frame. Since it's LIFO we move to an older frame.
+                    }
+                }
+            }
+        }
+        //If the request does not belong to the process move to the next process
+        else {
+            requestItr++;
+        }
+    }
 }
 
 void ldf()
