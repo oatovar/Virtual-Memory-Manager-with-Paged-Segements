cb41874408fa8aab07027aeadc4d997185a78882
Oscar Tovar <33381041+oatovar@users.noreply.github.com>
2017-12-01 14:59:17 -0600

    Update inputgen.cpp

--- inputgen.cpp
+++ inputgen.cpp
@@ -1,6 +1,5 @@
-/*
- * Generates valid memory requests.
- */
+//  InputGenerator
+//  Created by Oscar Tovar
 
 #include <iostream>
 #include <math.h>
@@ -12,9 +11,44 @@
 using namespace std;
 
 int main() {
-	srand(time(NULL));
-	for (int i = 0; i < 50; i++) {
-		cout << "Memory Request 0x" << std::hex << setw(2) << setfill('0') << rand() % 64 + 1 << setfill(' ') <<  endl;
-	}
-	return 0;
+    srand(time(NULL));
+    int tp = 20;//Total Page Frames in Memory
+    int sl = 5;//Segment Length in Pages
+    int ps = 32;//Page Size in Entries
+    int r = 5;//Pages per Process
+    int x = 3;//Look ahead value
+    int min = 14, max = 18;
+    int k = 4;//Number of processes to fork
+    int maxRequests[k];
+    int currentRequests[k];
+    int totalRequests = 0;
+    bool finishedProcesses[k];
+    
+    for (int i = 0; i < k; i++) {
+        currentRequests[i] = 0;
+        finishedProcesses[i] = false;
+        maxRequests[i] = rand() % 5 + 1;
+        totalRequests += maxRequests[i];
+    }
+    
+    
+    
+    for (int i = 0; i < k; i++) {
+        cout << 100 + i << " " << 32 << endl;
+    }
+    for (int i = 0; i < totalRequests + k; i++) {
+        int pid = 0;
+        do {
+           pid = (rand() % 4);
+        } while (finishedProcesses[pid] == true);
+        if (currentRequests[pid] < maxRequests[pid]) {
+            cout << std::dec << pid + 100 << " 0x" << std::hex << setw(2) << setfill('0') << rand() % (sl*ps) + 1 << setfill(' ') <<  endl;
+            currentRequests[pid] = currentRequests[pid] + 1;
+        }
+        else {
+            cout << std::dec << pid + 100 << " -1" << endl;
+            finishedProcesses[pid] = true;
+        }
+    }
+    return 0;
 }
