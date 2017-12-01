8b7f70e208f1ae688c5c6544408c22b4238b305f
Oscar <oscar.alberto.tovar@gmail.com>
2017-12-01 00:41:51 -0600

    Initial commit.

---
+++ structures.h
@@ -0,0 +1,25 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+using namespace std;
+
+struct processObject {
+	int pid = 0; //PID which is used as the address space/segment
+	int size = 0; //Max frames that can be allocated to address space
+	int currentFrameCount = 0; //Holds current frame count
+};
+
+struct memoryRequest {
+	int addressSpace = 0; //Address Space/PID
+	int segment = 0; //Segment #. In this case PID is the segment #.
+	int page = 0; //Page #
+	int offset = 0; //Offset
+};
+
+struct frame {
+	int addressSpace = 0; //Address Space Number/PID
+	int segment = 0; //Segment Number
+	int page = 0; //Page Number
+};
+
+#endif
