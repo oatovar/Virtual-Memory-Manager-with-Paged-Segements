0832de62100165ec8cfb32bf70cb146395a11631
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2017-12-02 15:12:33 -0600

    Correcting FIFO and LIFO to properly search for pages in memory.

--- algorithms.h
+++ algorithms.h
@@ -7,22 +7,41 @@
 
 using namespace std;
 
-void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, processObject processProperties[], int framesPerProcess, int processNum)
+void reset(list<frame> currentFrames, processObject processProperties[], int k)
 {
+	list<frame>::iterator frameItr = currentFrames.begin();
+	//Reset the frames table to simulate a fresh start
+	while(frameItr != currentFrames.end()) {
+		frameItr->addressSpace = 0;
+		frameItr->segment = 0;
+		frameItr->page = 0;
+		frameItr++;
+	}
+	//Reset process objects to reinitialize frame count and page faults to zero
+	for (int i = 0; i < k; i++) {
+		processProperties[i].pageFault = 0;
+		processProperties[i].currentFrameCount = 0;
+	}
+	return;
+}
+
+void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, processObject processProperties[], int framesPerProcess, int k)
+{
+	reset(currentFrames, processProperties, k);
+	cout << endl << "Starting FIFO replacement algorithm." << endl;
 	list<memoryRequest>::iterator requestItr = currentRequests.begin();
 	list<frame>::iterator frameItr = currentFrames.begin();
-	int addressSpace = requestItr->addressSpace - 100;
+	int totalPageFaults = 0;
+	//int addressSpace = requestItr->addressSpace - 100;
 	while (requestItr != currentRequests.end()) {
-        //If you run into the last request for the process just exit out
-        if ((requestItr->addressSpace == processNum) && (requestItr->offset == -1)) {
-            break;
-        }
-        //Check to see if the next request belongs to the process
-		else if ((requestItr->addressSpace == processNum) && (requestItr->offset != -1)) {
-			if (processProperties[addressSpace].currentFrameCount < framesPerProcess) {
+		if (requestItr->offset != -1) {
+			if (processProperties[requestItr->addressSpace - 100].currentFrameCount < framesPerProcess) {
 				while(frameItr != currentFrames.end()) {
 					if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
                         //Frame already contains page needed. No need to use a new frame buffer.
+                        cout << "Page in frame buffer." << endl;
+                        requestItr++;
+                        frameItr = currentFrames.begin();
                         break;
 					}
                     else if (frameItr->addressSpace == 0) {
@@ -32,8 +51,11 @@ void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, proces
                         frameItr->segment = requestItr->segment;
                         frameItr->page = requestItr->page;
                         //Increase the count of frames that currently belong to the process
-                        processProperties[addressSpace].currentFrameCount++;
+                        processProperties[requestItr->addressSpace - 100].currentFrameCount++;
+                        processProperties[requestItr->addressSpace - 100].pageFault++;
                         //Break out of loop and move on to the next request
+                        requestItr++;
+                        frameItr = currentFrames.begin();
                         break;
                     }
 					else {
@@ -41,32 +63,59 @@ void fifo(list<frame> currentFrames, list<memoryRequest> currentRequests, proces
 						frameItr++; //Advance to next frame.
 					}
 				}
-			} else {
-				while(frameItr != currentFrames.end()) {
-					if (frameItr->addressSpace == requestItr->addressSpace) {
-                        cout << "Replacing frame buffer." << endl;
-						//Remove the first page that came in
-                        frameItr = currentFrames.erase(frameItr);
-                        //Update frames table to reflect the new order of pages in frame buffers
-                        frame newFrame;
-                        newFrame.addressSpace = requestItr->addressSpace;
-                        newFrame.segment = requestItr->segment;
-                        newFrame.page = requestItr->page;
-                        currentFrames.push_back(newFrame);
-                        //Break out of loop and move on to the next request
-                        break;
+			}
+            else {
+            	frameItr = currentFrames.begin();
+            	while(frameItr != currentFrames.end()) {
+					if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
+						//Frame already contains page needed. No need to use a new frame buffer.
+						cout << "Page in frame buffer." << endl;
+						frameItr = currentFrames.begin();
+						requestItr++;
+						break;
 					}
 					else {
 						frameItr++;
 					}
 				}
-			}
+				frameItr = currentFrames.begin();
+				while(frameItr != currentFrames.end()) {
+					if (frameItr->addressSpace == requestItr->addressSpace) {
+						cout << "Replacing frame buffer." << endl;
+			            //Remove the first page that came in
+			            frameItr = currentFrames.erase(frameItr);
+			            //Update frames table to reflect the new order of pages in frame buffers
+			            frame newFrame;
+			            newFrame.addressSpace = requestItr->addressSpace;
+			            newFrame.segment = requestItr->segment;
+			            newFrame.page = requestItr->page;
+			            currentFrames.push_back(newFrame);
+			            processProperties[requestItr->addressSpace - 100].pageFault++;
+			            //Break out of loop and move on to the next request
+			            frameItr = currentFrames.begin();
+			            requestItr++;
+             		    break;
+            	    }
+            	    else {
+            	        frameItr++;//Go to the next frame. Since it's LIFO we move to an older frame.
+            	    }
+                }
+            }
 		}
         //If the request does not belong to the process move to the next process
         else {
+        	cout << "Process " << requestItr->addressSpace << " finished!" << endl;
+        	frameItr = currentFrames.begin();
 			requestItr++;
 		}
 	}
+	cout << "RESULTS" << endl;
+	for (int i = 0; i < k; i++) {
+		cout << "Page Faults for Process " << i + 100 << ": "
+			 << processProperties[i].pageFault << endl;
+		totalPageFaults += processProperties[i].pageFault;
+	}
+	cout << "Total Page Faults: " << totalPageFaults << endl;
 }
 
 void lru()
@@ -74,66 +123,101 @@ void lru()
 
 }
 
-void lifo(list<frame> currentFrames, list<memoryRequest> currentRequests, processObject processProperties[], int framesPerProcess, int processNum)
+void lifo(list<frame> currentFrames, list<memoryRequest> currentRequests, processObject processProperties[], int framesPerProcess, int k)
 {
+	reset(currentFrames, processProperties, k);
+	cout << endl << "Starting LIFO replacement algorithm." << endl;
     list<memoryRequest>::iterator requestItr = currentRequests.begin();
-    list<frame>::iterator frameItr = currentFrames.end();
-    int addressSpace = requestItr->addressSpace - 100;
+    list<frame>::iterator frameItr = currentFrames.begin();
+    int totalPageFaults = 0;
+    int index = 0;
+    //int addressSpace = requestItr->addressSpace - 100;
     while (requestItr != currentRequests.end()) {
-        //If you run into the last request for the process just exit out
-        if ((requestItr->addressSpace == processNum) && (requestItr->offset == -1)) {
-            break;
-        }
-        //Check to see if the next request belongs to the process
-        else if ((requestItr->addressSpace == processNum) && (requestItr->offset != -1)) {
-            if (processProperties[addressSpace].currentFrameCount < framesPerProcess) {
-                while(frameItr != currentFrames.begin()) {
+        if (requestItr->offset != -1) {
+            if (processProperties[requestItr->addressSpace - 100].currentFrameCount < framesPerProcess) {
+                while(frameItr != currentFrames.end()) {
                     if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
                         //Frame already contains page needed. No need to use a new frame buffer.
+                        index++;
+                        cout << "Index #" << index << " Page in frame buffer." << endl;
+                        frameItr = currentFrames.begin();
+                        requestItr++;
                         break;
                     }
                     else if (frameItr->addressSpace == 0) {
                         //Copy page to the frame buffer
-                        cout << "Copying page into frame buffer." << endl;
+                        index++;
+                        cout << "Index #" << index << " Copying page into frame buffer." << endl;
                         frameItr->addressSpace = requestItr->addressSpace;
                         frameItr->segment = requestItr->segment;
                         frameItr->page = requestItr->page;
                         //Increase the count of frames that currently belong to the process
-                        processProperties[addressSpace].currentFrameCount++;
+                        processProperties[requestItr->addressSpace - 100].currentFrameCount++;
+                        processProperties[requestItr->addressSpace - 100].pageFault++;
                         //Break out of loop and move on to the next request
+                        frameItr = currentFrames.begin();
+                        requestItr++;
                         break;
                     }
                     else {
                         //If frame occupied by another page either by the same process or another process
-                        frameItr--; //Advance to next frame. In this case moves on to an older frame.
+                        frameItr++; //Advance to next frame. In this case moves on to an older frame.
                     }
                 }
-            } else {
-                while(frameItr != currentFrames.begin()) {
-                    if (frameItr->addressSpace == requestItr->addressSpace) {
-                        cout << "Replacing frame buffer." << endl;
-                        //Remove the first page that came in
-                        frameItr = currentFrames.erase(frameItr);
-                        //Update frames table to reflect the new order of pages in frame buffers
-                        frame newFrame;
-                        newFrame.addressSpace = requestItr->addressSpace;
-                        newFrame.segment = requestItr->segment;
-                        newFrame.page = requestItr->page;
-                        currentFrames.push_front(newFrame);
-                        //Break out of loop and move on to the next request
-                        break;
-                    }
-                    else {
-                        frameItr--;//Go to the next frame. Since it's LIFO we move to an older frame.
-                    }
+            }
+            else {
+            	frameItr = currentFrames.begin();
+            	while(frameItr != currentFrames.end()) {
+					if ((frameItr->addressSpace == requestItr->addressSpace) && (frameItr->segment == requestItr->segment) && (frameItr->page == requestItr->page)) {
+						//Frame already contains page needed. No need to use a new frame buffer.
+						index++;
+						cout << "Index #" << index << " Page in frame buffer." << endl;
+						frameItr = currentFrames.begin();
+						requestItr++;
+						break;
+					}
+					else {
+						frameItr++;
+					}
+				}
+				frameItr = currentFrames.begin();
+				while(frameItr != currentFrames.end()) {
+					if (frameItr->addressSpace == requestItr->addressSpace) {
+						cout << "Replacing frame buffer." << endl;
+			            //Remove the first page that came in
+			            frameItr = currentFrames.erase(frameItr);
+			            //Update frames table to reflect the new order of pages in frame buffers
+			            frame newFrame;
+			            newFrame.addressSpace = requestItr->addressSpace;
+			            newFrame.segment = requestItr->segment;
+			            newFrame.page = requestItr->page;
+			            currentFrames.push_front(newFrame);
+			            processProperties[requestItr->addressSpace - 100].pageFault++;
+			            //Break out of loop and move on to the next request
+			            frameItr = currentFrames.begin();
+			            requestItr++;
+             		    break;
+            	    }
+            	    else {
+            	        frameItr++;//Go to the next frame. Since it's LIFO we move to an older frame.
+            	    }
                 }
             }
         }
         //If the request does not belong to the process move to the next process
         else {
+        	cout << "Process " << requestItr->addressSpace << " finished!" << endl;
+        	frameItr = currentFrames.begin();
             requestItr++;
         }
     }
+	cout << "RESULTS" << endl;
+	for (int i = 0; i < k; i++) {
+		cout << "Page Faults for Process " << i + 100 << ": "
+			 << processProperties[i].pageFault << endl;
+		totalPageFaults += processProperties[i].pageFault;
+	}
+	cout << "Total Page Faults: " << totalPageFaults << endl;
 }
 
 void ldf()
